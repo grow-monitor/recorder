d10018e5cb73257e548f91b0ca53cb440455555d
zegheim <justin@aristakas.in>
2021-04-11 18:04:20 +0100

    implement insertion functionality

--- record.py
+++ record.py
@@ -1,8 +1,9 @@
+import json
+
 import paho.mqtt.client as mqtt
 import psycopg2
-from pgcopy import CopyManager
 
-import config
+from config import DB_URL, HOST, PASSWORD, PORT, TABLE, USERNAME
 
 
 class Recorder(object):
@@ -13,6 +14,7 @@ class Recorder(object):
         self.client.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLS)
         self.client.username_pw_set(username, password)
         self.client.connect(host, port)
+        self.conn = psycopg2.connect(DB_URL)
 
     def on_connect(self, client, userdata, flags, rc):
         if rc == 0:
@@ -21,35 +23,34 @@ class Recorder(object):
             print("Connect returned result code: {}".format(rc))
 
     def on_message(self, client, userdata, msg):
+        data = self.parse(msg)
+        self.insert(data)
+
+    def parse(self, msg):
         topic = msg.topic.split("/")[-1]
-        payload = msg.payload
-        print(f"Received message {topic} -> {payload}")
+        payload = json.loads(msg.payload)
+        return (payload["timestamp"], topic, payload["value"])
 
     def subscribe(self, topics):
         result, message_id = self.client.subscribe(topics)
         return result, message_id
+
+    def insert(self, data):
+        cur = self.conn.cursor()
+        query = f"INSERT INTO {TABLE} (timestamp, topic, value) VALUES (?, ?, ?);"
+        try:
+            cur.execute(query, data)
+        except (Exception, psycopg2.Exception) as err:
+            print(f"Unable to insert data: {err}")
+        self.conn.commit()
 
     def run(self):
         self.client.loop()
 
 
 if __name__ == "__main__":
-    recorder = Recorder(config.USERNAME, config.PASSWORD, config.HOST, config.PORT)
+    recorder = Recorder(USERNAME, PASSWORD, HOST, PORT)
     recorder.subscribe([("mock/moisture", 2), ("mock/saturation", 2)])
     while True:
         recorder.run()
-
-
-# with psycopg2.connect(config.DB_URL) as conn:
-#     SQL = """
-#     SELECT  generate_series(now() - interval '24 hour', now(), interval '5 minute') AS timestamp,
-#     %s as topic,
-#     random() AS value
-#     """
-#     cur = conn.cursor()
-#     for topic in ("mock/saturation", "mock/moisture"):
-#         data = (topic,)
-#         cur.execute(SQL, data)
-#         values = cur.fetchall()
-#         print(values)
 
