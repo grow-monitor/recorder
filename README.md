f152a5b1db0c126b9c53d86c0c2cc6f0203eba1a
zegheim <justin@aristakas.in>
2021-04-11 19:10:59 +0100

    add installation script

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ A service to continuously record sensor data from an MQTT Broker
 ## Dependencies
 
 - `psycopg2` has a dependency on `pg_config`, which you can install by running `sudo apt install libpq-dev` (Debian / Ubuntu).
+
+## Recording as a service
+
+- Copy `service/grow-recorder.service.example` to `service/grow-recorder.service` and fill in the blanks.
+- Run `./service/install.sh` to install the recorder as a background task.
---
+++ service/install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+user_check() {
+	if [ $(id -u) -ne 0 ]; then
+		printf "Script must be run as root. Try 'sudo ./install.sh'\n"
+		exit 1
+	fi
+}
+
+success() {
+	echo -e "$(tput setaf 2)$1$(tput sgr0)"
+}
+
+inform() {
+	echo -e "$(tput setaf 6)$1$(tput sgr0)"
+}
+
+warning() {
+	echo -e "$(tput setaf 1)$1$(tput sgr0)"
+}
+
+user_check
+
+inform "Installing systemd service...\n"
+cp grow-recorder.service /etc/systemd/system/
+systemctl reenable grow-recorder.service
+systemctl start grow-recorder.service
+
+inform"\nSuccessfully installed grow-recorder as a systemd service.\n"
